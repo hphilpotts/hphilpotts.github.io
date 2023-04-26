683f5df55e05fbb5d102b3f952b068d83f681853
Henry Philpotts <hphilpotts@gmail.com>
2023-04-26 12:28:29 +0100

    Nav links highlight by scroll position, wavy text added to projects landing page.

--- main.js
+++ main.js
@@ -1,24 +1,71 @@
 // Navigation
 
-const scrollToTargetElement = targetElement => targetElement.scrollIntoView({ behavior: "smooth" });
-
-class LinkGroup {
+class FullPage {
     constructor(link) {
         this.links = [document.getElementById(link + '-link'), document.getElementById(link + '-link-mobile'), document.getElementById(link + '-span')]
         this.targetScreen = document.getElementById(link + '-screen')
+        this.roundedTargetTop = Math.floor(this.targetScreen.getBoundingClientRect().top / 100)
 
         const target = this.targetScreen
         const filteredLinks = this.links.filter(Boolean)
-        filteredLinks.forEach(link => link.addEventListener('click', function () { scrollToTargetElement(target) }))
+        filteredLinks.forEach(link => link.addEventListener('click', function () { 
+            scrollToTargetElement(target)
+            checkCurrentScrollPosition()
+        }))
     }
 }
 
-const projectsLinks = new LinkGroup('projects')
-const aboutLinks = new LinkGroup('about')
-const helloLinks = new LinkGroup('hello')
+const scrollToTargetElement = targetElement => targetElement.scrollIntoView({ behavior: "smooth" });
+
+const projectsLinks = new FullPage('projects')
+const aboutLinks = new FullPage('about')
+const helloLinks = new FullPage('hello')
 
 const downArrow = document.getElementById('down-arrow')
 downArrow.addEventListener('click', function () { scrollToTargetElement(helloLinks.targetScreen) })
+
+
+// throttler for scroll events
+
+const throttledScrollEvent = (func, delay) => {
+    let time = Date.now()
+
+    return () => {
+        if ((time + delay - Date.now()) <= 0) {
+            func()
+            time = Date.now()
+        }
+    }
+}
+
+
+// Update nav link highlights
+const mainContainer = document.getElementById('main-container')
+
+const checkCurrentScrollPosition = () => {
+    const currentScrollPointTop = Math.floor(mainContainer.scrollTop / 100)
+
+    console.log(currentScrollPointTop, aboutLinks.roundedTargetTop);
+
+    switch (currentScrollPointTop) {
+        case helloLinks.roundedTargetTop:
+            $(helloLinks.links.filter(Boolean)).addClass('highlighted')
+            break;
+        case projectsLinks.roundedTargetTop:
+            $(projectsLinks.links).addClass('highlighted')
+            break;
+        case aboutLinks.roundedTargetTop:
+            $(aboutLinks.links).addClass('highlighted')
+            break;
+        default:
+            $(helloLinks.links.filter(Boolean)).removeClass('highlighted')
+            $(projectsLinks.links).removeClass('highlighted')
+            $(aboutLinks.links).removeClass('highlighted')
+    }   
+}
+
+mainContainer.addEventListener('scroll', throttledScrollEvent(checkCurrentScrollPosition, 200))
+
 
 // Project Slider Behaviour
 
@@ -53,7 +100,7 @@ const illuminateIconsAtIndeces = [
     [0, 1, 2, 15],
 ]
 
-slider.addEventListener('scroll', function () {
+const handleSliderScroll = () => {
     const currentSlide = checkAllSliderLefts()
     const currentIlluminations = illuminateIconsAtIndeces[currentSlide]
     if (currentSlide === undefined) {
@@ -63,7 +110,9 @@ slider.addEventListener('scroll', function () {
             $(`[index=${index}]`).fadeIn(250)
         })
     }
-})
+}
+
+slider.addEventListener('scroll', throttledScrollEvent(handleSliderScroll, 10))
 
 const slideBody = {};
 
@@ -112,6 +161,6 @@ const toggleAboutDiv = buttonClicked => {
     targetElement.toggle()
 }
 
-aboutButtons.on("click", function () {
+aboutButtons.on("click", function() {
     toggleAboutDiv($(this))
 })
